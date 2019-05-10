9d4290d12184b1bb17c1613d7f265036a26f1a26
Federico Zimmermann <48495525+fedezimm@users.noreply.github.com>
2019-05-10 18:42:57 -0300

    Actividad N1 - Grupo 4

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        
+
+        static void Main(string[] args)
+        {
+
+            using (var ctx = new Model())
+            {
+                var stud = new Student() { StudentName = "Sebastián" };
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+
+
+         //   using (var ctx1 = new Model())
+         //    {
+         //      var Lista = ctx1.Students
+         //           .Where(x=>x.StudentID > 2);
+         //       foreach(var stud2 in Lista)
+         //           { Console.WriteLine(stud2.StudentName);
+         //           stud2.Height = 1.45M;
+         //       }
+                
+         //       ctx1.SaveChanges();
+         //   }
+            
+        }
+
+
+
+
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/model.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+
+{
+    
+        public class Model : DbContext
+        {
+
+            public DbSet<Student> Students { get; set; }
+            public DbSet<Grade> Grades { get; set; }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=per(nombre);Trusted_Connection=True;");
+            }
+        }
+        public class Student
+        {
+            public int StudentID { get; set; }
+            public string StudentName { get; set; }
+            public DateTime? DateOfBirth { get; set; }
+            public byte[] Photo { get; set; }
+            public decimal Height { get; set; }
+            public float Weight { get; set; }
+
+            public Grade Grade { get; set; }
+        }
+        public class Grade
+        {
+            public int GradeId { get; set; }
+            public string GradeName { get; set; }
+            public string Section { get; set; }
+
+            public ICollection<Student> Students { get; set; }
+        }
+   
+}
